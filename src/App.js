854163a55c778bbb837e1c50708a12e3e61279f2
Ehren <ehren.chen.ej@gmail.com>
2021-06-02 01:57:41 +0800

    [.App.js/ListCard.js ] 把清單元件拆出來

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import styled from '@emotion/styled';
 
+// Component
+import ListCard from './view/ListCard';
+
 const Container = styled.div`
   /* background-color: red; */
   height: 100%;
@@ -18,40 +21,6 @@ const Wallpapaer = styled.div`
   min-width: 320px;
   padding-top: 10px;
   
-`;
-
-const ListCard = styled.div`
-  background-color: white;
-  width: 100%;
-  height: calc(100vh - 110px);
-  /* display: flex;
-  justify-content: center; */
-  overflow-y:scroll;
-`;
-
-const ListItem = styled.div`
-  /* width: 90%; */
-  /* background-color: yellowgreen; */
-  border: 1px solid lightgrey;
-  border-radius: 2px;
-  height: 100px;
-  margin: 0 10px 10px 10px;
-  padding:5px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const ItemImg = styled.div`
-  width: 40%;
-  background-color: lightpink;
-  height: 100%;
-`;
-
-const ItemName = styled.div`
-  background-color: greenyellow;
-  width: 60%;
-  margin-left: 5px;
-  height: 100%;
 `;
 
 const ToolBox = styled.div`
@@ -98,12 +67,7 @@ const App = () =>{
   return (
     <Container>
       <Wallpapaer>
-        <ListCard>
-          <ListItem>
-            <ItemImg/>
-            <ItemName/>
-          </ListItem>
-        </ListCard>
+        <ListCard/>
         <ToolBox>
           <AddBtn>
             <AddIcon/>
---
+++ src/view/ListCard.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from '@emotion/styled';
+const ListCardBlock = styled.div`
+  background-color: white;
+  width: 100%;
+  height: calc(100vh - 110px);
+  /* display: flex;
+  justify-content: center; */
+  overflow-y:scroll;
+`;
+
+const ListItem = styled.div`
+  /* width: 90%; */
+  /* background-color: yellowgreen; */
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  height: 100px;
+  margin: 0 10px 10px 10px;
+  padding:5px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ItemImg = styled.div`
+  width: 40%;
+  background-color: lightpink;
+  height: 100%;
+`;
+
+const ItemName = styled.div`
+  background-color: greenyellow;
+  width: 60%;
+  margin-left: 5px;
+  height: 100%;
+`;
+
+const ListCard = () => {
+
+
+  return(
+    <ListCardBlock>
+      <ListItem>
+        <ItemImg/>
+        <ItemName/>
+      </ListItem>
+    </ListCardBlock>
+  );
+
+};
+
+export default ListCard;
